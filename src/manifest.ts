e65d7d5f59b46e4159b70d5f2188f2c6fa5cd754
xxg <38970638+XXGGG@users.noreply.github.com>
2026-01-15 21:55:47 +0800

    🚑 图片小修改

--- src/manifest.ts
+++ src/manifest.ts
@@ -13,18 +13,18 @@ export async function getManifest() {
     description: pkg.description,
     action: {
       default_icon: {
-        16: './assets/icon-16.png',
-        48: './assets/icon-48.png',
-        128: './assets/icon-128.png',
+        16: 'assets/icon-16.png',
+        48: 'assets/icon-48.png',
+        128: 'assets/icon-128.png',
       },
-      default_popup: './dist/popup/index.html',
+      default_popup: 'dist/popup/index.html',
     },
     options_ui: {
-      page: './dist/options/index.html',
+      page: 'dist/options/index.html',
       open_in_tab: true,
     },
     chrome_url_overrides: {
-      newtab: './dist/newtab/index.html',
+      newtab: 'dist/newtab/index.html',
     },
     background: isFirefox
       ? {
@@ -32,12 +32,12 @@ export async function getManifest() {
           type: 'module',
         }
       : {
-          service_worker: './dist/background/index.mjs',
+          service_worker: 'dist/background/index.mjs',
         },
     icons: {
-      16: './assets/icon-16.png',
-      48: './assets/icon-48.png',
-      128: './assets/icon-128.png',
+      16: 'assets/icon-16.png',
+      48: 'assets/icon-48.png',
+      128: 'assets/icon-128.png',
     },
     permissions: [
       'tabs',
